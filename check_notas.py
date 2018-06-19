aae2db189e817877b6fae90caf268f89507fbbae
Ruben <ruben.p.nogueira@gmail.com>
2018-06-19 20:12:57 +0100

    Little Dummy test

--- check_notas.py
+++ check_notas.py
@@ -7,8 +7,9 @@ WAITING = 10 #10 minutos
 UCS = [
         {'uc':'SO','url':'https://ead.ipleiria.pt/2017-18/course/view.php?id=3659'},
         {'uc':'P2','url':'https://ead.ipleiria.pt/2017-18/course/view.php?id=3711'},
-        {'uc':'Estatistica','url':'https://ead.ipleiria.pt/2017-18/course/view.php?id=3688'}
-      ]
+        {'uc':'Dummy','url':'https://ead.ipleiria.pt/2017-18/course/view.php?id=6969'},
+        {'uc':'Outra','url':'https://ead.ipleiria.pt/2017-18/course/view.php?id=1234'},
+        ]
 
 hashpath = os.path.join(sys.path[0],'hashfiles.dat')
 s = requests.Session()
